fce4917eea208b6e8c258e02d456ecfaef6a9244
Pascal <pascal.grones10@gmail.com>
2019-12-13 23:04:20 +0100

    Added the ability to share the IP by holding the text

--- app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
+++ app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
@@ -4,11 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.github.jan222ik.boardgamenotepad.server.Server
 import kotlinx.android.synthetic.main.activity_main.*
 import android.text.method.ScrollingMovementMethod
 import com.github.jan222ik.boardgamenotepad.server.ServerService
-
+import com.github.jan222ik.boardgamenotepad.utils.Utils
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,20 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         playerNamesLabel.movementMethod = ScrollingMovementMethod()
-
-        startService(Intent(this, ServerService::class.java))
+        ipLabel.setOnLongClickListener {
+            val s = ipLabel.text
+            val sharingIntent = Intent(Intent.ACTION_SEND)
+            sharingIntent.type = "text/plain"
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, s)
+            startActivity(Intent.createChooser(sharingIntent, "Share text via"))
+            return@setOnLongClickListener true
+        }
     }
 
     fun startGame(view: View) {
-        ipLabel.text = Server().getIPAddress(true)
+        startService(Intent(this, ServerService::class.java))
+        ipLabel.text = Utils.getIPAddress(true)
     }
 
     fun stopGame(view: View) {
+        stopService(Intent(this, ServerService::class.java))
         ipLabel.text = ""
     }
 
     fun resetGame(view: View) {
-
+        stopService(Intent(this, ServerService::class.java))
+        startService(Intent(this, ServerService::class.java))
     }
 
 }
