06278492a03dbe379661a29eebccad1f147e6728
Pascal <pascal.grones10@gmail.com>
2019-12-14 00:17:19 +0100

    Added disabled buttons and implemented onClickListeners

--- app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
+++ app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
@@ -3,14 +3,12 @@ package com.github.jan222ik.boardgamenotepad
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.text.method.ScrollingMovementMethod
 import com.github.jan222ik.boardgamenotepad.server.ServerService
 import com.github.jan222ik.boardgamenotepad.utils.Utils
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var intentField : Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         intentField = Intent(this, ServerService::class.java)
         playerNamesLabel.movementMethod = ScrollingMovementMethod()
+
         ipLabel.setOnLongClickListener {
             val s = ipLabel.text
             val sharingIntent = Intent(Intent.ACTION_SEND)
@@ -25,21 +24,31 @@ class MainActivity : AppCompatActivity() {
             sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
             sharingIntent.putExtra(Intent.EXTRA_TEXT, s)
             startActivity(Intent.createChooser(sharingIntent, "Share text via"))
-            return@setOnLongClickListener true
+            true
         }
+
+        startBtn.setOnClickListener { startGame() }
+        stopBtn.setOnClickListener { stopGame() }
+        resetBtn.setOnClickListener { resetGame() }
     }
 
-    fun startGame(view: View) {
+    fun startGame() {
         startService(intentField)
         ipLabel.text = Utils.getIPAddress(true)
+        stopBtn.isEnabled = true
+        resetBtn.isEnabled = true
+        startBtn.isEnabled = false
     }
 
-    fun stopGame(view: View) {
+    fun stopGame() {
         stopService(intentField)
         ipLabel.text = ""
+        stopBtn.isEnabled = false
+        resetBtn.isEnabled = false
+        startBtn.isEnabled = true
     }
 
-    fun resetGame(view: View) {
+    fun resetGame() {
         stopService(intentField)
         startService(intentField)
     }
