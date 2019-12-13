b8b87fb0689ef0edaa19667cfa69c5b8251f4f88
Pascal <pascal.grones10@gmail.com>
2019-12-13 22:43:08 +0100

    UI and MainActivity functions

--- app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
+++ app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.github.jan222ik.boardgamenotepad
 
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.github.jan222ik.boardgamenotepad.server.Server
+import kotlinx.android.synthetic.main.activity_main.*
+import android.text.method.ScrollingMovementMethod
 import com.github.jan222ik.boardgamenotepad.server.ServerService
 
 
@@ -11,6 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playerNamesLabel.movementMethod = ScrollingMovementMethod()
+
         startService(Intent(this, ServerService::class.java))
     }
+
+    fun startGame(view: View) {
+        ipLabel.text = Server().getIPAddress(true)
+    }
+
+    fun stopGame(view: View) {
+        ipLabel.text = ""
+    }
+
+    fun resetGame(view: View) {
+
+    }
+
 }
