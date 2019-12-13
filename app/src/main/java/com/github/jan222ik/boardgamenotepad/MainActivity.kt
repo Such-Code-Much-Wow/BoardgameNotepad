3058eb671470d056d953fa38a78dfcd585f8773a
Janik Mayr <jan.222.ik@gmail.com>
2019-12-13 18:26:32 +0100

    Initial

---
+++ app/src/main/java/com/github/jan222ik/boardgamenotepad/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.jan222ik.boardgamenotepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
