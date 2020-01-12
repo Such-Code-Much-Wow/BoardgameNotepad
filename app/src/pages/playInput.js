b496f1244fe8b0836f36440a022ec59e42be4687
Matthias <mle2266@students.fhv.at>
2020-01-12 21:14:49 +0100

    more unicorns

--- app/src/pages/playInput.js
+++ app/src/pages/playInput.js
@@ -4,13 +4,40 @@ function handlePlayerInput() {
         let request = new XMLHttpRequest();
         let url = "msg";
         url = "http://homepages.fhv.at/mle2266/vote.php"; //for test purposes
+
+        //delete this after testing:
+        var called = false;
+
         request.onreadystatechange = function(){
-          console.log("yes");
+            console.log(arguments);
+            if(this.readyState === 4 && this.status === 200){
+            }
+            if(!called) {
+                handleResult();
+                called = true;
+            }
         };
         request.open("POST", url);
         request.send(input);
     }
 
+}
+function handleResult() {
+    var htmlList = document.getElementById("resultList");
+    console.log("Y U DO DAT");
+    var listData = ["get", "on", "my", "level", "peasant"];
+    for (var i = 0; i < listData.length; ++i) {
+        // create an item for each one
+        var listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = listData[i];
+
+        // Add listItem to the listElement
+        htmlList.appendChild(listItem);
+    }
+    document.getElementById("inputDiv").hidden = true;
+    document.getElementById("resultDiv").hidden = false;
 }
 
 function handlePlayerJoin(){
