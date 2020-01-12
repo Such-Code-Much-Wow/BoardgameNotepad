86a0c1c32bcbc4c5dcfa932741e9b469acd1a91f
Janik Mayr <jan.222.ik@gmail.com>
2020-01-12 16:42:50 +0100

    Added Vote state

--- app/src/main/java/com/github/jan222ik/boardgamenotepad/game/GameState.kt
+++ app/src/main/java/com/github/jan222ik/boardgamenotepad/game/GameState.kt
@@ -1,5 +1,5 @@
 package com.github.jan222ik.boardgamenotepad.game
 
 enum class GameState {
-    JOIN, SELECT_MODERATOR, ENTER_TEXT, MODERATOR_PEEK, REVEAL
+    JOIN, SELECT_MODERATOR, ENTER_TEXT, MODERATOR_PEEK, VOTING, REVEAL
 }
