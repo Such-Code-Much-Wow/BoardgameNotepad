21a89b230f4a3abbfc94207c7da1d2452dcd89e0
Pascal <pascal.grones10@gmail.com>
2020-01-12 22:43:46 +0100

    Polling works

--- app/src/pages/playInput.js
+++ app/src/pages/playInput.js
@@ -1,6 +1,46 @@
+let gameState = null;
+let isMod = false;
+
+function poll() {
+    let request = new XMLHttpRequest();
+    let url = "msg";
+    console.log("poll called");
+    request.onreadystatechange = function () {
+        if (this.status === 200) {
+            console.log(arguments);
+            //todo: updatet gaemsatea
+            if (gameState !== "pollState") {
+                switch (gameState) {
+                    case "JOIN":
+                        showDiv("joinDiv");
+                        break;
+                    case "ENTER_TEXT":
+                        showDiv("inputDiv");
+                        break;
+                    case "MODERATOR_PEEK":
+                        if (isMod)
+                            showDiv("resultDiv");
+                        break;
+                    case "REVEAL":
+                        showDiv("resultDiv");
+                        break;
+                }
+            }
+            console.log("poll");
+        }
+    };
+
+    request.open("GET", url);
+    request.send();
+}
+
+function sleep(time) {
+    return new Promise((resolve => setInterval(resolve, time)));
+}
+
 function handlePlayerInput() {
     let input = document.getElementById("playerInput").value;
-    if (input && input.length > 0){
+    if (input && input.length > 0) {
         let request = new XMLHttpRequest();
         let url = "msg";
         url = "http://homepages.fhv.at/mle2266/vote.php"; //for test purposes
@@ -8,23 +48,16 @@ function handlePlayerInput() {
         //delete this after testing:
         var called = false;
 
-        request.onreadystatechange = function(){
-            console.log(arguments);
-            if(this.readyState === 4 && this.status === 200){
-            }
-            if(!called) {
-                handleResult();
-                called = true;
-            }
-        };
+        // request.onreadystatechange = onRequestCallback;
         request.open("POST", url);
         request.send(input);
+        handlePending("Please Wait");
     }
 
 }
+
 function handleResult() {
     var htmlList = document.getElementById("resultList");
-    console.log("Y U DO DAT");
     var listData = ["get", "on", "my", "level", "peasant"];
     for (var i = 0; i < listData.length; ++i) {
         // create an item for each one
@@ -36,27 +69,41 @@ function handleResult() {
         // Add listItem to the listElement
         htmlList.appendChild(listItem);
     }
-    document.getElementById("inputDiv").hidden = true;
-    document.getElementById("resultDiv").hidden = false;
+    showDiv("resultDiv");
 }
 
-function handlePlayerJoin(){
+function handlePlayerJoin() {
     let name = document.getElementById("nameInput").value;
 
-    if(name && name.length > 0) {
+    if (name && name.length > 0) {
         let request = new XMLHttpRequest();
-        let url = "game/join?realname=$name";
+        let url = "game/join?realname=" + name;
         //url = "http://homepages.fhv.at/mle2266/vote.php"; //for test purposes
-        request.onreadystatechange = function(){
+        request.onreadystatechange = function () {
             console.log("yes");
         };
         request.open("GET", url);
         request.send();
 
-        document.getElementById("joinDiv").hidden = true;
-        document.getElementById("inputDiv").hidden = false;
-    }
-    else{
+        showDiv("inputDiv");
+    } else {
         alert("GIVE ME A NAME PEASANT")
     }
+}
+
+function handlePending(value) {
+    document.getElementById("pendingText").value = value;
+    showDiv("pendingDiv");
+}
+
+function handleContinueGame() {
+    //send to rest to go to next state
+}
+
+function showDiv(div) {
+    let divs = document.getElementsByTagName("div");
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].hidden = true;
+    }
+    document.getElementById(div).hidden = false;
 }
